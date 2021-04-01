8e2a9da0b476a1e2596547a28cb10c2a0e64225c
raoul <git@raoulkramer.de>
2021-04-01 17:18:00 +0200

    hmmm

--- main.js
+++ main.js
@@ -2,26 +2,13 @@ import './style.css'
 
 const host = import.meta.env.VITE_APP_HOST;
 
-console.log(host);
-
-document.addEventListener('click', (event) => {
-  if (event.target.matches('a[href]:not([target="_blank"])')) {
-    event.preventDefault();
-    const url = new URL(event.target.href);
-    window.history.pushState(null, '', `#${url.pathname}`);
-  }
-});
-
-window.addEventListener('hashchange', (event) => {
-  console.log(event);
-});
-
 function loadPage() {
-  fetch(`${host}${document.location.hash.substr(1)}`)
+  fetch(`${host}${document.location.pathname}`)
   .then((res) => res.text())
   .then((bodyText) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(bodyText, 'text/html');
+    document.title = `/vit/ - ${doc.querySelector('head title').textContent}`;
     document.querySelector('#app').innerHTML = doc.querySelector('body').innerHTML;
   });
 }
